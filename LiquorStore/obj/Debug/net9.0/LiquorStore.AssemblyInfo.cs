473b3fa077363d632b942bbe34d2d9fac4bd6af7
avi-ls <avinash.sadhai@hotmail.ca>
2025-04-14 03:15:22 -0400

    Added some bootstrap css to the homepage

--- LiquorStore/obj/Debug/net9.0/LiquorStore.AssemblyInfo.cs
+++ LiquorStore/obj/Debug/net9.0/LiquorStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30cc3f55a50ec35a11fd298e616a7145acdf948")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576914a642a9519892db33d39bdec72106f7c496")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
