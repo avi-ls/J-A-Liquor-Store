ab34a5ef26eb5f36303cd826055f343977accd32
avi-ls <avinash.sadhai@hotmail.ca>
2025-04-14 02:14:58 -0400

    Fixed some issues with navbar

--- LiquorStore/obj/Debug/net9.0/LiquorStore.AssemblyInfo.cs
+++ LiquorStore/obj/Debug/net9.0/LiquorStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc3027b4eedcd9a973a2a16c486c06bc3975950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417b268e5666dba287005bb33c1a040867ee6b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
