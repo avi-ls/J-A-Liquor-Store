576914a642a9519892db33d39bdec72106f7c496
avi-ls <avinash.sadhai@hotmail.ca>
2025-04-14 02:52:38 -0400

    Added new products to the database

--- LiquorStore/obj/Debug/net9.0/LiquorStore.AssemblyInfo.cs
+++ LiquorStore/obj/Debug/net9.0/LiquorStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417b268e5666dba287005bb33c1a040867ee6b22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30cc3f55a50ec35a11fd298e616a7145acdf948")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiquorStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
